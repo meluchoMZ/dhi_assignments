6f2263807ba59edd1452467ab2024403c6b98789
meluchoMZ <miguelgodon@outlook.es>
2021-03-04 10:48:38 +0100

    Changed led behaviour

--- p1/p1.ino
+++ p1/p1.ino
@@ -13,13 +13,15 @@ void setup(void)
 	pinMode(button, INPUT);
 	pinMode(green, OUTPUT);
 	pinMode(red, OUTPUT);
+	Serial.println("Benvido ao xogo dos reflexos!");
+	Serial.println("Prema o boton para comezar o xogo");
+	randomSeed(analogRead(0));
 }
 
 void loop(void)
 {
 	int del, count, start, time;
-	Serial.println("Benvido ao xogo dos reflexos!");
-	Serial.println("Prema o boton para comezar o xogo");
+
 	digitalWrite(green, HIGH);
 	while(digitalRead(button) != HIGH)
 	{
@@ -32,49 +34,46 @@ void loop(void)
 		;
 	}
 
-	while(true)
+	del = random(400, 1201);
+	count = del;
+	start = millis();
+	// pooling cada 10ms mentres non se pulse o boton ou pase o tempo
+	while(digitalRead(button) != HIGH && count > 0)
 	{
-		del = random(400, 1201);
-		count = del;
-		start = millis();
-		// pooling cada 10ms mentres non se pulse o boton ou pase o tempo
-		while(digitalRead(button) != HIGH && count > 0)
-		{
-			delay(1);
-			count--;
-		}
-		// so se encende o led vermello se se fixo a espera completa
-		if (count <= 0) {
-			digitalWrite(red, HIGH);
-			// esperase a que pulsen o led vermello
-			while(digitalRead(button) !=  HIGH) 
-			{
-				;
-			}
-			digitalWrite(red, LOW);
-		}
-		// calculanse os tempos
-		time = millis()-start;
-		// esperase depulsarse
-		while (digitalRead(button) != LOW) 
+		delay(1);
+		count--;
+	}
+	// so se encende o led vermello se se fixo a espera completa
+	if (count <= 0) {
+		digitalWrite(red, HIGH);
+		// esperase a que pulsen o led vermello
+		while(digitalRead(button) !=  HIGH) 
 		{
 			;
 		}
-		// imprimese por porto serie
-		if (time <= del) {
-			Serial.println("Non se precipite. Tente de novo");
-		} else {
-			if ((time - del) < 100) {
-				Serial.print(time - del, DEC);
-				Serial.println("ms PARABENS! Excelentes reflexos");
-			} else if ((time-del) > 200) {
-				Serial.print(time - del, DEC);
-				Serial.println("ms MOI LENTO! Tente de novo");
-			} else {
-				Serial.print(time - del, DEC);
-				Serial.println("ms BEN! Bos reflexos");
-			}
-		}
-		delay(50);
+		digitalWrite(red, LOW);
 	}
+	// calculanse os tempos
+	time = millis()-start;
+	// esperase depulsarse
+	while (digitalRead(button) != LOW) 
+	{
+		;
+	}
+	// imprimese por porto serie
+	if (time <= del) {
+		Serial.println("Non se precipite. Tente de novo");
+	} else {
+		if ((time - del) < 100) {
+			Serial.print(time - del, DEC);
+			Serial.println("ms PARABENS! Excelentes reflexos");
+		} else if ((time-del) > 200) {
+			Serial.print(time - del, DEC);
+			Serial.println("ms MOI LENTO! Tente de novo");
+		} else {
+			Serial.print(time - del, DEC);
+			Serial.println("ms BEN! Bos reflexos");
+		}
+	}
+	delay(50);
 }
